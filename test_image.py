319ef589f4f085edd089a4922b120bf2e207cd3d
KK Kim <314127900@qq.com>
2017-12-28 11:53:56 +0800

    Update test_image.py
    set default cpu

--- test_image.py
+++ test_image.py
@@ -7,7 +7,7 @@ import dface.core.vision as vision
 
 if __name__ == '__main__':
 
-    pnet, rnet, onet = create_mtcnn_net(p_model_path="./model_store/pnet_epoch.pt", r_model_path="./model_store/rnet_epoch.pt", o_model_path="./model_store/onet_epoch.pt", use_cuda=True)
+    pnet, rnet, onet = create_mtcnn_net(p_model_path="./model_store/pnet_epoch.pt", r_model_path="./model_store/rnet_epoch.pt", o_model_path="./model_store/onet_epoch.pt", use_cuda=False)
     mtcnn_detector = MtcnnDetector(pnet=pnet, rnet=rnet, onet=onet, min_face_size=24)
 
     img = cv2.imread("./test.jpg")
