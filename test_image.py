f1b82e0b970eeca1ac931f7c4fbf242e3912fb62
Kaustubh "Kauby" Sakhalkar <sakhalkark@gmail.com>
2018-03-22 14:03:01 +0100

    Use OpenCV color converter

--- test_image.py
+++ test_image.py
@@ -11,8 +11,9 @@ if __name__ == '__main__':
     mtcnn_detector = MtcnnDetector(pnet=pnet, rnet=rnet, onet=onet, min_face_size=24)
 
     img = cv2.imread("./test.jpg")
-    b, g, r = cv2.split(img)
-    img2 = cv2.merge([r, g, b])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #b, g, r = cv2.split(img)
+    #img2 = cv2.merge([r, g, b])
 
     bboxs, landmarks = mtcnn_detector.detect_face(img)
     # print box_align
