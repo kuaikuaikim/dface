cd900fc7295457084cda01f91e0b9eb128d4e4bd
KK Kim <314127900@qq.com>
2018-05-23 23:12:29 +0800

    Update test_image.py
    no bgr to rgb

--- test_image.py
+++ test_image.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     mtcnn_detector = MtcnnDetector(pnet=pnet, rnet=rnet, onet=onet, min_face_size=24)
 
     img = cv2.imread("./test.jpg")
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_bg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     #b, g, r = cv2.split(img)
     #img2 = cv2.merge([r, g, b])
 
     bboxs, landmarks = mtcnn_detector.detect_face(img)
     # print box_align
 
-    vision.vis_face(img2,bboxs,landmarks)
+    vision.vis_face(img_bg,bboxs,landmarks)
